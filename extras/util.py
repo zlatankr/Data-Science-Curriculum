716b6a70bbc121ee28360bf798d26c3ab3a154c7
zlatankr <zlatankr@gmail.com>
2017-06-02 18:21:00 -0700

    start RNN module

--- extras/util.py
+++ extras/util.py
@@ -32,3 +32,24 @@ def getKaggleMNIST():
 
 def init_weights(shape):
     return np.random.randn(*shape) / np.sqrt(sum(shape))
+
+    
+def init_weight(Mi, Mo):
+    return np.random.randn(Mi, Mo) / np.sqrt(Mi + Mo)
+
+def all_parity_pairs_with_sequence_labels(nbit):
+    X, Y = all_parity_pairs(nbit)
+    N, t = X.shape
+
+    # we want every time step to have a label
+    Y_t = np.zeros(X.shape, dtype=np.int32)
+    for n in xrange(N):
+        ones_count = 0
+        for i in xrange(t):
+            if X[n,i] == 1:
+                ones_count += 1
+            if ones_count % 2 == 1:
+                Y_t[n,i] = 1
+
+    X = X.reshape(N, t, 1).astype(np.float32)
+    return X, Y_t
